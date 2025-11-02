6c83ed79685b96deb12eadb4c99d7bea4cd4046c
tuan6100 <tuananhho378@gmail.com>
2025-11-02 19:05:24 +0700

    add react plugin

--- forge.config.js
+++ forge.config.js
@@ -33,12 +33,12 @@ module.exports = {
         build: [
           {
             // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-            entry: 'src/main.js',
+            entry: 'src/main/main.js',
             config: 'vite.main.config.mjs',
             target: 'main',
           },
           {
-            entry: 'src/preload.js',
+            entry: 'src/main/preload.js',
             config: 'vite.preload.config.mjs',
             target: 'preload',
           },
